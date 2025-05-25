f39335260ec68958162a04554bebf893e7984327
ashishcharde <ashishcharde09@gmail.com>
2025-05-25 14:57:49 +0530

    second change

--- javacode/azurecode.java
+++ javacode/azurecode.java
@@ -3,6 +3,11 @@ package javacode;
 public class azurecode {
 
     public static void main(String[] args) {
+
         System.out.println("hello world! from azure devops");
+
+        // second change --add second sysoutsf
+        System.out.println("hello world! second change");
+
     }
 }
